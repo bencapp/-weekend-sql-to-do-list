5c041e19608ee8e7bd0a233c5c22b2021e3a79a7
Ben Capp <bentucker333@gmail.com>
2023-02-03 12:35:52 -0600

    completed and tested GET endpoint

--- server/server.js
+++ server/server.js
@@ -8,6 +8,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("server/public"));
 
+// DATABASE CONNECTION SETUP
+const pg = require("pg");
+
+const pool = new pg.Pool({
+  host: "localhost",
+  port: 5432,
+  database: "weekend-to-do-app",
+});
+
+//        GET ENDPOINT
+// --------------------------- //
+app.get("/list", (req, res) => {
+  let queryText = `SELECT * FROM "toDoList"`;
+  pool
+    .query(queryText)
+    .then((dbResponse) => {
+      res.send(dbResponse.rows);
+    })
+    .catch((error) => {
+      console.log("GET request failed", error);
+      res.sendStatus(500);
+    });
+});
+
+//        POST ENDPOINT
+// --------------------------- //
+
+//         PUT ENDPOINT
+// --------------------------- //
+
+//       DELETE ENDPOINT
+// --------------------------- //
+
 //         PORT SETUP
 // --------------------------- //
 const PORT = process.env.PORT || 5000;
