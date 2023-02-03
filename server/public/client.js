3ad8d344127e8ee7f691ac2e17de872fb015a014
Ben Capp <bentucker333@gmail.com>
2023-02-03 13:16:45 -0600

    completed list get and render function

--- server/public/client.js
+++ server/public/client.js
@@ -2,4 +2,39 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log("jquery loaded successfully");
+
+  // create event listeners
+  $("#add-btn").on("click", onAddTask);
+
+  getList();
+}
+
+function onAddTask(event) {
+  // prevent automatic reload
+  event.preventDefault();
+
+  console.log("adding task");
+}
+
+function getList() {
+  $.ajax({ type: "get", url: `/list` })
+    .then((response) => {
+      console.log("getting list; list is:", response);
+      renderList(response);
+    })
+    .catch((error) => {
+      console.log("Error in client GET:", error);
+    });
+}
+
+// list parameter is an array of objects send from the database
+function renderList(list) {
+  $("#to-do-list").empty();
+  for (let taskObject of list) {
+    $("#to-do-list").append(`
+        <tr>
+            <td>${taskObject.task}</td>
+            <td>${taskObject.completed}</td>
+        </tr>`);
+  }
 }
