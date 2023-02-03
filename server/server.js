4832dd411abe3a03eae22372994715bdad24208f
Ben Capp <bentucker333@gmail.com>
2023-02-03 12:04:19 -0600

    set up server and client files

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve back static files by default
+app.use(express.static("server/public"));
+
+console.log("Server running");
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("server running, ready for access at", PORT);
+});
