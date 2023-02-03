336bcd19302b33ba19a74b179747b8621915bf84
Ben Capp <bentucker333@gmail.com>
2023-02-03 12:12:53 -0600

    set upp database and updated database.sql

---
+++ database.sql
@@ -0,0 +1,13 @@
+-- CREATE INITIAL TABLE
+CREATE TABLE "toDoList" (
+    "id" serial PRIMARY KEY,
+    "task" varchar(80) NOT NULL,
+    "completed" BOOLEAN DEFAULT FALSE
+);
+
+-- DUMMY DATA
+INSERT INTO "toDoList" (task)
+VALUES ('Vaccuum'),
+('Grocery shopping'),
+('Brush teeth'),
+('Take a nap');
--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
+//         SERVER SETUP
+// --------------------------- //
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve back static files by default
 app.use(express.static("server/public"));
 
-console.log("Server running");
-
+//         PORT SETUP
+// --------------------------- //
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("server running, ready for access at", PORT);
